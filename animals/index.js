771696c6307085b045d217eaae0ba2848684d262
Mateo <rugalbernstein.2002@proton.me>
2025-06-20 15:45:22 +0200

    Added touch support for infoboxes

--- animals/index.js
+++ animals/index.js
@@ -14,4 +14,26 @@ document.querySelectorAll(".has-extra").forEach(element => {
         event.target.style.textDecoration = "dotted underline 2px";
         currentInfobox = null;
     }, false);
+    /* add support for touch devices.
+    concurrency is not an issue, since mouse and touch events
+    will not trigger on the same device,
+    therefore, only one block of code will use `currentInfobox`. */
+    element.addEventListener("touchend", event => {
+        // an infobox is currently showing; hide it
+        if (currentInfobox) {
+            currentInfobox.setAttribute("class", "extra-hidden");
+            event.target.style.textDecoration = "dotted underline 2px";
+            currentInfobox = null;
+        }
+        // no infobox is showing; show the target's associated infobox
+        else {
+            event.target.style.textDecoration = "blue solid underline 2px";
+            const {top, left} = getComputedStyle(event.target);
+            const infobox = event.target.nextElementSibling;
+            infobox.setAttribute("class", "extra-visible");
+            infobox.style.top = `${top - (0.25 * infobox.clientHeight)}px`;
+            infobox.style.left = `${left - (0.25 * infobox.clientWidth)}px`;
+            currentInfobox = infobox;
+        }
+    });
 });
