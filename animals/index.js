d4e3e00d991adcde79b536f0401498423d06f074
Mateo <rugalbernstein.2002@proton.me>
2025-06-20 20:20:36 +0200

    Fixed infobox bug
    On pages with multiple infoboxes, occasionally, the wrong infobox would close or `TypeError: currentInfoBox is null` would be thrown.

--- animals/index.js
+++ animals/index.js
@@ -1,4 +1,3 @@
-let currentInfobox;
 document.querySelectorAll(".has-extra").forEach(element => {
     element.addEventListener("mouseover", event => {
         event.target.style.textDecoration = "none";
@@ -7,23 +6,22 @@ document.querySelectorAll(".has-extra").forEach(element => {
         infobox.setAttribute("class", "extra-visible");
         infobox.style.top = `${top - (0.25 * infobox.clientHeight)}px`;
         infobox.style.left = `${left - (0.25 * infobox.clientWidth)}px`;
-        currentInfobox = infobox;
     }, false);
     element.addEventListener("mouseout", event => {
+        const currentInfobox = document.querySelector(".extra-visible");
         currentInfobox.setAttribute("class", "extra-hidden");
         event.target.style.textDecoration = "dotted underline 2px";
-        currentInfobox = null;
     }, false);
     /* add support for touch devices.
     concurrency is not an issue, since mouse and touch events
     will not trigger on the same device,
     therefore, only one block of code will use `currentInfobox`. */
     element.addEventListener("touchend", event => {
+        const currentInfobox = document.querySelector(".extra-visible");
         // an infobox is currently showing; hide it
         if (currentInfobox) {
             currentInfobox.setAttribute("class", "extra-hidden");
             event.target.style.textDecoration = "dotted underline 2px";
-            currentInfobox = null;
         }
         // no infobox is showing; show the target's associated infobox
         else {
@@ -33,7 +31,6 @@ document.querySelectorAll(".has-extra").forEach(element => {
             infobox.setAttribute("class", "extra-visible");
             infobox.style.top = `${top - (0.25 * infobox.clientHeight)}px`;
             infobox.style.left = `${left - (0.25 * infobox.clientWidth)}px`;
-            currentInfobox = infobox;
         }
     });
 });
